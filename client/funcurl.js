9dd8dddd539170e467cc52f087f3df487cca8af0
DavidPopovich3261 <8534397@gmail.com>
2025-12-16 13:56:39 +0200

    local

---
+++ client/funcurl.js
@@ -0,0 +1,81 @@
+import input from "analiza-sync";
+
+export async function get(name, lang) {
+    try {
+        const res = await fetch(`http://localhost:3000/greet?name=${name}&lang=${lang}`)
+        const data = await res.json()
+        console.log(data);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+export async function post(numbers) {
+    try {
+        const res = await fetch("http://localhost:3000/math/average", {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({ "numbers": numbers })
+        })
+        const data = await res.json()
+        console.log(data);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function put(word) {
+    try {
+        const res = await fetch(`http://localhost:3000/shout/${word}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "put",
+        })
+        const data = await res.json()
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function delete_demo(x_role) {
+    try {
+        const res = await fetch("http://localhost:3000/secure/resource",
+            {
+                method: "delete",
+                headers: {
+                    'Content-Type': 'application/json',
+                    "x-role": x_role
+                }
+            })
+        const data = await res.json()
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+async function all() {
+    let name = input("name\n")
+    let lang = input("lang\n")
+    let numbers = input("numbers\n")
+    numbers = numbers.split(" ")
+    let ward = input("ward\n")
+    let x_role = input("user\n")
+    Promise.all([
+        get(name, lang),
+        post(numbers),
+        put(ward),
+        delete_demo(x_role)
+    ])
+
+}
+all()
---
+++ client/menu.js
@@ -0,0 +1,37 @@
+import { get,post,put,delete_demo} from "./funcurl.js";
+import input from "analiza-sync"
+
+while(true){
+    let caches = input(`1. Generate a greeting\n2. Calculate the average\n3. Transform a word\n4. Delete a protected\n5. Call each server endpoint one after the other (in sequence)\n6. Call all server endpoints in parallel\n7. Exit\n`)
+    switch (caches) {
+        case "1":
+            let name = input("name\n")
+            let lang = input("lang\n")
+            await get(name,lang)
+            break;
+
+        case "2":
+            let numbers = input ("numbers\n")
+            numbers = numbers.split(" ")
+            await post(numbers)
+            break;
+
+        case "3":
+            let ward = input("ward\n")
+            await put(ward)
+            break;
+
+        case "4":
+            let x_role = input("user\n")
+            await delete_demo(x_role)
+            break;
+
+        case "5":
+
+            break;
+    
+        default:
+            break;
+    }
+    
+}
